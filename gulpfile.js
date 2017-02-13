01bf2cff2259a9e1f210700cebb7f40504306ed7
Dennis <dennis.antela@gmail.com>
2017-02-13 13:11:25 -0600

    installed postcss and its plugins autoprefixer and postcss-simple-vars

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
-var gulp 	= require('gulp'),
-	watch	= require('gulp-watch');
+var gulp 			= require('gulp'),
+	watch			= require('gulp-watch'),
+	postcss			= require('gulp-postcss'),
+	autoprefixer 	= require('autoprefixer'),
+	cssvars			= require('postcss-simple-vars');
 
 gulp.task('default', function() {
 	console.log("Hello World");
+});
+
+gulp.task('styles', function() {
+	return gulp.src('./app/assets/styles/styles.css')
+		.pipe(postcss([cssvars, autoprefixer]))
+		.pipe(gulp.dest('./app/temp/styles'));
 });
 
 gulp.task('watch', function(){
@@ -11,7 +20,7 @@ gulp.task('watch', function(){
 	});
 
 	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start(console.log("dummy text"))
+		gulp.start('styles');
 	});
 });
 
